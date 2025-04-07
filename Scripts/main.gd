c70e955e40c819e530e4ce6cb20d4be2f98c3dd9
Cameron <camermcgill11@yahoo.com>
2025-04-07 17:43:23 -0400

    hopefully file creation

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -11,5 +11,9 @@ func _on_quit_pressed() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_play_pressed() -> void:
+	var path = "user://LIST.txt"
+	var file = FileAccess.open(path, FileAccess.READ_WRITE)
+	if not FileAccess.file_exists(path):
+		file = FileAccess.open(path, FileAccess.WRITE)
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 	
