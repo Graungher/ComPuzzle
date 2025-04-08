a0532a0a3c17e97b1d22daaa440acbbcea63ffaf
Cameron <camermcgill11@yahoo.com>
2025-04-08 13:07:25 -0400

    play scene

--- Scripts/play.gd
+++ Scripts/play.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-
-
+@onready var warnWindow = $Warning_window
+@onready var menu = $Panel
 
 
 func _on_continue_pressed() -> void:
 	var curMap = mapList.getCurrentMap()
+	if curMap == -1:
+		curMap = 0
 	mapList.select_map(curMap)
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 	pass # Replace with function body.
@@ -26,4 +28,28 @@ func _on_new_game_pressed() -> void:
 	var file = FileAccess.open(path, FileAccess.WRITE)
 	file.store_line(str(1))
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	pass # Replace with function body.
+
+
+func _show_warning_window() -> void:
+	warnWindow.popup()
+	pass # Replace with function body.
+
+
+func _on_warning_window_canceled() -> void:
+	pass # Replace with function body.
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	pass # Replace with function body.
+
+
+func _on_menu_button_pressed() -> void:
+	menu.visible = not menu.visible
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
 	pass # Replace with function body.
