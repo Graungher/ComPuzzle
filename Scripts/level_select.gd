31aa2b73f05c9de433bc6f468f4fadcef3e1499e
Abzy4 <abzy4@icloud.com>
2025-04-09 22:23:20 -0400

    couple thangs

--- Scripts/level_select.gd
+++ Scripts/level_select.gd
@@ -34,3 +34,8 @@ func unlockButtons():
 func _on_ready() -> void:
 	unlockButtons()
 	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/play.tscn")
+	pass # Replace with function body.
